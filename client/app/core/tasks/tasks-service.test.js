00dabedc4cbbdb3ea0e0243348cd69aa0ab117dd
Nick Van Weerdenburg <nick@rangle.io>
2014-09-08 21:57:35 -0400

    fixed broken test

--- client/app/core/tasks/tasks-service.test.js
+++ client/app/core/tasks/tasks-service.test.js
@@ -1,3 +1,33 @@
+describe('tasks service 1', function () {
+    // Load the angular module. Having smaller modules helps here.
+    beforeEach(module('erg'));
+    it('should get tasks', function(done) {
+      // Notice that we've specified that our function takes a 'done' argument.
+      // This tells Mocha this is an asynchronous test. An asynchronous test will
+      // not be considered 'successful' until done() is called without any
+      // arguments. If we call done() with an argument the test fails, treating
+      // that argument as an error.
+      inject(function (tasks, $rootScope, $httpBackend) {
+        $httpBackend.expectGET('http://ngcourse.herokuapp.com/api/v1/tasks').respond([{ test: 'true' }]);
+        tasks.getTasks()
+          // Attach the handler for resolved promise.
+          .then(function (tasks) {
+            // Assertions thrown here will result to a failed promise downstream.
+            expect(tasks).to.be.an.array;
+            // Remember to call done(), othewise the test will time out (and
+            // fail).
+            done();
+          })
+          // Attach the error handler. This is very important and easy to forget.
+          .then(null, function(error) {
+            done(error); // This can be simplified - see below.
+          });
+          $httpBackend.flush();
+          $rootScope.$apply();
+      });
+    });
+  });
+
 describe('tasks service', function () {
     // Load the angular module. Having smaller modules helps here.
     beforeEach(module('erg'));
