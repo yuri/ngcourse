742c4f28f0a1e3797e438de8d3695e162ffd0c78
Evan <evan@rangle.io>
2014-09-16 09:43:43 -0400

    Updating gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,48 @@ var gulp = require('gulp');
 var rg = require('rangle-gulp');
 
 var karmaVendorFiles = [
-  'client/bower_components/angular/angular.min.js',
-  'client/bower_components/angular-mocks/angular-mocks.js',
-  'client/bower_components/sinon-chai/lib/sinon-chai.js',
-  'client/testing/lib/q.js',
-  'client/testing/test-utils.js'
+    'client/bower_components/angular/angular.js',
+    'client/bower_components/angular-mocks/angular-mocks.js',
+    'client/bower_components/sinon-chai/lib/sinon-chai.js',
+    'client/testing/lib/q.js',
+    'client/testing/test-utils.js'
 ];
 
 gulp.task('karma', rg.karma({
-  // files: specify which folders (optional)
-  // karmaConf: specify which karma config file (optional)
-  vendor: karmaVendorFiles
+    // files: specify which folders (optional)
+    // karmaConf: specify which karma config file (optional)
+    vendor: karmaVendorFiles
 }));
 
 gulp.task('karma-watch', rg.karmaWatch({
-  // files: specify which folders (optional)
-  // karmaConf: specify which karma config file (optional)
+    // files: specify which folders (optional)
+    // karmaConf: specify which karma config file (optional)
 }));
 
 gulp.task('mocha', rg.karmaWatch({
-  // files: specify which folders (optional)
+    // files: specify which folders (optional)
 }));
 
 gulp.task('lint', rg.jshint({
-  // files: specify which files (optional)
+    // files: specify which files (optional)
 }));
 
 gulp.task('beautify', rg.beautify({
-  // files: specify which files (optional)
+    // files: specify which files (optional)
 }));
 
 gulp.task('concat', rg.concatAndUglify({
-  // files: specify which files (optional)
-  // name: specify what the result should be called, e.g. 'foo'
-  // dist: specify where the output should go, e.g. 'client/dist/'
+    // files: specify which files (optional)
+    // name: specify what the result should be called, e.g. 'foo'
+    // dist: specify where the output should go, e.g. 'client/dist/'
 }));
 
 gulp.task('dev', rg.connectWatch({
-  root : 'client',
-  port : 8080,
-  livereload : true,
-  // Files to watch for live re-load
-  glob : ['./client/**/*.html', './client/**/*.js']
+    root: 'client',
+    port: 8080,
+    livereload: true,
+    // Files to watch for live re-load
+    glob: ['./client/**/*.html', './client/**/*.js']
 }));
 
 gulp.task('default', ['lint', 'concat', 'mocha', 'karma']);
