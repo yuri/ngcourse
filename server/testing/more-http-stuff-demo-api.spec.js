e1359de42148a23a0fed010333ab2bfc6b664f6a
Seth Davenport <seth@rangle.io>
2015-01-16 14:17:41 -0500

    E2E fix and update based on supertest blog example.
    Got rid of a broken test - when we delete a task, the server returns an empty object instead of 404.
    
    This wasn't caught because of a missing return statement in the test.
    
    I also added a multi-scenario test inspired by work on the supertest blog post.

--- server/testing/more-http-stuff-demo-api.spec.js
+++ server/testing/more-http-stuff-demo-api.spec.js
@@ -45,13 +45,27 @@ describe('Different things you can do with HTTP', function() {
       });
   });
 
-  it ('Should be able to DELETE', function() {
-    return request.delete('/api/v1/tasks/' + newTaskId)
+  it ('Can test multi-request scenarios', function() {
+    var newTaskId;
+    var jsonPayload = { owner: 'alice', description: 'Write some tests!' };
+
+    // Create a task.
+    return request.post('/api/v1/tasks')
+      .send(jsonPayload)
       .expect(200)
-      .then(function() {
-        // Make sure it's actually gone.
-        request.get('/api/v1/tasks/' + newTaskId)
-          .expect(404);
+      .then(function(createResponse) {
+        newTaskId = JSON.parse(createResponse.text)[0]._id;
+        assert(!!newTaskId, 'got a new task ID');
+
+        // Make sure we can get the newly created task.
+        return request.get('/api/v1/tasks/' + newTaskId).expect(200);
+      })
+      .then(function(getResponse) {
+        var id = JSON.parse(getResponse.text)[0]._id;
+        assert(newTaskId === id, 'got the same task ID back again');
+
+        // Delete the task when we're done.
+        return request.delete('/api/v1/tasks/' + newTaskId).expect(200);
       });
   });
 });
