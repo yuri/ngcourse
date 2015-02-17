230acce363409a96cd2532d7f5343fa2b97dcb02
Seth Davenport <seth@rangle.io>
2015-02-17 18:02:10 -0500

    Prepared base branch for 2015-02-18 session.

--- client/app/app.js
+++ client/app/app.js
@@ -1,28 +1,3 @@
 'use strict';
 
-angular.module('ngcourse', [
-  'ngcourse.main-ctrl',
-  'ngcourse.tasks',
-  'ngcourse.users',
-  'ngcourse.router',
-  'koast',
-  'ngcourse-example-directives'
-])
-
-// .constant('API_BASE_URL', 'http://localhost:7000')
-.constant('API_BASE_URL', 'http://ngcourse.herokuapp.com')
-
-.run(function ($log, koast, API_BASE_URL) {
-  $log.info('All ready!');
-
-  koast.init({
-    baseUrl: API_BASE_URL
-  });
-  koast.setApiUriPrefix('/api/v2/');
-  koast.addEndpoint('tasks', ':_id', {
-    useEnvelope: true
-  });
-  koast.addEndpoint('users', ':_id', {
-    useEnvelope: true
-  });
-});
+angular.module('ngcourse', []);
